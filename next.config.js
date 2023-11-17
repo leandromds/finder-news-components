4fd40993854b2b6f17a50b986d9039b71e63733a
leandromds <leandro.mds.21@gmail.com>
2023-11-17 12:14:37 -0300

    chore: enable cors-origin

--- next.config.js
+++ next.config.js
@@ -2,13 +2,26 @@
 const NextFederationPlugin = require('@module-federation/nextjs-mf')
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true,async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   webpack(config, options) {
     const { isServer } = options
     config.experiments = { 
       topLevelAwait: true,
       layers: true
-    }
+    },
     config.plugins.push(
       new NextFederationPlugin({
         name: 'finderNewsComponents',
