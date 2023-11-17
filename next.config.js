902fdec4890033f05da4664930e8b5300c904582
leandromds <leandro.mds.21@gmail.com>
2023-11-17 06:07:25 -0300

    chore: setup inicial project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf')
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options
+    config.experiments = { 
+      topLevelAwait: true,
+      layers: true
+    }
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'finderNewsComponents',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './header': './src/components/Header.tsx',
+        },
+        shared: {},
+      }),
+    )
+    return config
+  },
 }
 
 module.exports = nextConfig
